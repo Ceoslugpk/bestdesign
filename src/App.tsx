1e4f79dd096a11b68e2406395fe9d581f91a0e19
Ceoslugpk <ceo@slug.org.pk>
2025-05-27 10:24:57 +0400

    Start repository

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Page components
+import HomePage from './pages/HomePage';
+import HostingPlansPage from './pages/HostingPlansPage';
+import DomainsPage from './pages/DomainsPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
+// Layout components
+import MainLayout from './layouts/MainLayout';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="hosting" element={<HostingPlansPage />} />
+          <Route path="domains" element={<DomainsPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
